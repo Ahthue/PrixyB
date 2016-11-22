82fdb830c5248464c0d241b0c545e03fa45ffa83
Unknown <ludo.1610@hotmail.fr>
2016-11-22 16:44:30 +0100

    Modif Footer + New Nav

--- footer.php
+++ footer.php
@@ -1,6 +1,44 @@
 <!--FOOTER-->
-<div class="footer" style="margin-top:100px;">
+<div class="bg_footer">
+	<div class="footer container" style="margin-top:100px;">
+	  <div class="row">
+	  	<div class="col-lg-6 class_footer footer1">
+	  		<div class="row row_footer">
+	  			> Développé pour la société Prixy Formation
+	  		</div>
+	  		<div class="row row_footer">
+	  			> &copy; Copyright by <a href="#">Ell@Sio</a>
+	  		</div>
+	  	</div>
+	  	<div class="col-lg-6 class_footer text-right">
+	  		<div class="row row_footer">
+	        <a class="nav-link" href="#" title="Facebook">
+	          <i class="fa fa-facebook fa-2x"></i>
+	        </a>
+	        <a class="nav-link"  href="#" title="Twitter">
+	          <i class="fa fa-twitter fa-2x"></i>
+	        </a> 
+	        <a class="nav-link"  href="#" title="LinkedIn">
+	          <i class="fa fa-linkedin fa-2x"></i>
+	        </a> 
+	        <a class="nav-link" href="#" title="Youtube">
+	          <i class="fa fa-youtube fa-2x"></i>
+	        </a>
+	        <a class="nav-link" href="#" title="Pinterest">
+	          <i class="fa fa-pinterest-p fa-2x last_i"></i>
+	        </a>
+	  		</div>
+	  		<div class="row row_footer">
+	  			<a href="/PrixyPPE/mentions.php">Obligations légales</a>
+	  		</div>
+	  	</div>
+	  </div>		
+	</div>
+</div>
+
+<!--<div class="footer" style="margin-top:100px;">
   <nav class="navbar navbar-default navbar-bottom">
+  	
           <h4><?php print T_("Développé pour la société Prixy Formation"); ?> | &copy; Copyright by <a href="#">Ell@Sio</a> | <a href="/PrixyPPE/mentions.php"><?php print T_("Obligations légales"); ?></a> </h4>
   </nav>		
-</div>
+</div> -->
