6c538725a8f8095611b405600a56421997e5350b
Unknown <ludo.1610@hotmail.fr>
2016-12-08 14:13:11 +0100

    Add Profil + Nav

---
+++ profil.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<title>Prixy | Formation</title>
+  	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="bootstrap/img/favicon.png">
+    <script src="bootstrap/js/jquery-1.12.1.min.js"></script>
+	  <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!--
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/fontawesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="bootstrap/css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+
+<?php include('testnav.php'); ?>
+    
+
+<!--PRESENTATION-->
+
+
+<div class="container input-co text-center">
+  <h1>Modifier votre profil</h1>
+
+  <!-- formulaire d'inscription -->
+  <form action="inscription.php" method = "post">
+
+    <!-- Zone de texte pour le mail, l'adresse, le cp et la ville --> 
+    
+      <div class="input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+        <span class="input-group-addon" id="basic-addon3">Adresse mail</span>
+        <input type="email" class="form-control" name = "mail" maxlength = "100" placeholder = "Exemple : bono.jean@monmail.com" required = "required" value = "">
+      </div>
+
+      <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+        <span class="input-group-addon" id="basic-addon3">Adresse</span>
+        <input type="text" class="form-control" name = "adr" maxlength = "25" placeholder = "Veuillez insérer votre adresse ici." required = "required" value = "">
+      </div>
+
+        <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+          <div class="input-group">
+              <span class="input-group-addon" id="basic-addon3">CP</span>
+            <input type="text" class="form-control" name = "cp" maxlength = "25" placeholder = "Veuillez insérer votre code postale ici." required = "required" value = "">
+          </div>
+        </div>
+
+        <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon3">Ville</span>
+            <input type="text" class="form-control" name = "ville" maxlength = "25" placeholder = "Veuillez insérer le nom de votre ville d'habitation ici." required = "required" value = "">
+            </div>
+        </div>
+
+        <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon3">Tel. fixe</span>
+            <input type="tel" class="form-control" name = "tel_fixe" maxlength = "15" placeholder = "Votre numéro de téléphone fixe ici" required = "required" value = "">
+            </div>
+        </div>
+      
+      <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon3">Tel. mobile</span>
+            <input type="tel" class="form-control" name = "tel_mobile" maxlength = "15" placeholder = "Votre numéro de portable ici" value = "">
+            </div>
+        </div>
+
+    <!-- Zone de texte pour le login, le mdp et la confimation de mdp --> 
+
+      <div class="input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+        <span class="input-group-addon" id="basic-addon3">Login</span>
+        <input type="text" class="form-control" name = "login" maxlength = "25" placeholder = "Veuillez entrer votre pseudonyme de connexion ici." required = "required" value = "" >
+      </div>
+
+      <hr>
+
+      <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+        <span class="input-group-addon" id="basic-addon3">Mot de passe *</span>
+        <input type="password" class="form-control" name = "mdp" maxlength = "25" pattern="^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$" title = "Le mot de passe requiert 8 caractères min., 1 majuscule min., une minuscule min. et un nombre." placeholder = "Veuillez insérer votre mot de passe dans ce champ." required = "required">
+      </div>
+      <p class = "row col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 text-left">* Au moins 8 caractères avec au moins une majuscule, une minuscule et un nombre</p>
+      
+      
+      <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">            
+        <span class="input-group-addon" id="basic-addon3">Confirmer MdP</span>
+        <input type="password" class="form-control" name = "conf_mdp" maxlength = "25" pattern="^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$" title = "Le mot de passe requiert 8 caractères min., 1 majuscule min., une minuscule min. et un nombre." placeholder = "Confirmer votre mot de passe." required = "required">
+      </div>
+      <hr>
+      <!-- Bouton de validation du formulaire d'inscription --> 
+      <div class="row input-group col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+        <div class="col-lg-6">
+          <input type="submit" name="validerInscription" class="form-control okForm" value="Valider" style="margin-bottom:10px;">
+        </div>
+        <div class="col-lg-6">
+          <input type="submit" name="validerInscription" class="form-control okForm" value="Annuler" style="margin-bottom:10px;">
+        </div>
+      </div>
+  </form>
+</div>
+
+<?php include ('footer.php'); ?>
+	
+
+<script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); 
+});
+</script>
+
+	</body>
+</html>
--- testnav.php
+++ testnav.php
@@ -1,4 +1,3 @@
-<?php include 'fonction.inc.php'; ?>
 <div class="navbar-header navbar-fixed-top navbar-inverse">
   <button data-toggle="collapse" data-target="#items_nav_container" class="navbar-toggle">
       <span class="sr-only">Afficher le menu</span>
@@ -6,7 +5,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
   </button>
-  <h2 class="navbar-brand bgh2">PRIXY</h2>
+  <a href="index.php"><h2 class="navbar-brand bgh2">PRIXY</h2></a>
   <div id="items_nav_container" class="items_nav collapse navbar-collapse">
     <a class="item_nav" href="/PrixyPPE/index.php">
       <span class="glyphicon glyphicon-home"> <b>Accueil</b></span>
@@ -19,29 +18,30 @@
     </a>
     <a class="item_nav" href="/PrixyPPE/contact.php">
       <span class="glyphicon glyphicon-user"> <b>Contact &amp; Partenaires</b></span>
-    </a>    
+    </a>
+    <!-- <a class="item_nav" href="#" title="Facebook">
+    <i class="fa fa-facebook fa-lg"></i>
+    </a>
+    <a class="item_nav"  href="#" title="Twitter">
+    <i class="fa fa-twitter fa-lg"></i>
+    </a>
+    <a class="item_nav"  href="#" title="LinkedIn">
+    <i class="fa fa-linkedin fa-lg"></i>
+    </a>
+    <a class="item_nav" href="#" title="Youtube">
+    <i class="fa fa-youtube fa-lg"></i>
+    </a>
+    <a class="item_nav" href="#" title="Pinterest">
+    <i class="fa fa-pinterest-p fa-lg"></i>
+    </a> -->
     <div class="buttons_grp">
       <div class="btn-group group_nav pull-right" role="group">
-		<?php
-			if(isset($_SESSION['pseudonyme']))
-			{
-				echo '<a href="profil.php" class="btn btn-md btn-primary" role="button">'.$_SESSION['pseudonyme'].'</a>';
-				echo '<a href="deconnexion.php" class="btn btn-md btn-primary" role="button">Deconnexion</a>';
-			}
-			else
-			{
-				echo '<a href="connexion.php" class="btn btn-md btn-primary" role="button">Connexion</a>';
-				echo '<a href="inscription.php" class="btn btn-md btn-primary" role="button">Inscription</a>';
-			}
-		?>		
-        
+        <a href="connexion.php" class="btn btn-md btn-primary" role="button">Connexion</a>
+        <a href="inscription.php" class="btn btn-md btn-primary" role="button">Inscription</a>
       </div>
-      <div class="dropdown group_nav2 pull-right">
-        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Langue <span class="caret"></span></button>
-        <ul class="dropdown-menu" role="menu">
-          <li><a href="?lang=fr_FR">Fr</a></li>
-          <li><a href="?lang=en_US">An</a></li>
-        </ul>
+      <div class="btn-group group_nav2 pull-right" role="group">
+        <a href="#" class="btn btn-md btn-primary" role="button">Français</a>
+        <a href="#" class="btn btn-md btn-primary" role="button">&nbsp; English &nbsp;</a>
       </div>
     </div>
   </div>
